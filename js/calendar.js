1460777535a65b3e3410cf9e3d53f1fe49dc5b4b
Evelyn He <evelynhe2003@gmail.com>
2022-05-14 23:25:33 -0400

    First ideas for the calendar

---
+++ js/calendar.js
@@ -0,0 +1,14 @@
+// function createCalendar() {
+
+//     for (var i = 0; i <= 16; i++) {
+//         for (var j = 1; j <= 7; j++) {
+//             var div = document.createElement("div");
+//             div.style.width="100%";
+//             div.style.length="100%";
+//             div.style.background="red";
+//             document.getElementById("calendar").appendChild(div);
+//         }
+//         var jump=document.createElement("br");
+//         document.getElementById("calendar").appendChild(jump);
+//     }
+// }
